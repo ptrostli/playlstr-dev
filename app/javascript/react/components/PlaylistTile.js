2ebab0c6575f0d666e1b921b43895e5d92a413ec
Philip Trostli <philiptrostli@gmail.com>
2022-11-18 15:28:05 -0500

    attempt to attach username to playlist on index

--- app/javascript/react/components/PlaylistTile.js
+++ app/javascript/react/components/PlaylistTile.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+// TO COMPLETE --> Display associated username.
 const PlaylistTile = (props) => {
-
+  // debugger
   return (
     <div className="playlist-tile">
       {/* <Link to={`/playlists/${playlist.id}`}> */}
         <h4>{props.playlist.title}</h4>
+        <p>{props.playlist.description}</p>
+        
       {/* </Link> */}
     </div>
   )
--- app/serializers/playlist_serializer.rb
+++ app/serializers/playlist_serializer.rb
@@ -1,5 +1,5 @@
 class PlaylistSerializer < ActiveModel::Serializer
-  attributes :id, :title
+  attributes :id, :title, :description
 
   belongs_to :user
   has_many :songs
