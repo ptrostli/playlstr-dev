24c5854071bb207370a5ecc9f8f93fd613009961
Philip Trostli <philiptrostli@gmail.com>
2022-11-25 22:26:26 -0500

    navbar update

--- app/javascript/react/components/LoginButtonsTile.js
+++ app/javascript/react/components/LoginButtonsTile.js
@@ -5,9 +5,12 @@ const LoginButtonsTile = (props) => {
 
   return (
     <div className="login-options">
-      <p>LoginButtonsTile</p>
+      {/* <p>LoginButtonsTile</p>
       <Link to="/users/sign_in">Log In</Link>
       <Link to="/users/sign_up">Sign Up</Link>
+      <Link to='Sign Up' new_user_registration_path></Link> */}
+
+     
     </div>
   )
 }
