6ffdd7dd437851a8e858f0e73a8617fcf993f6d0
Philip Trostli <philiptrostli@gmail.com>
2022-11-28 12:20:13 -0500

    render deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,7 +5,6 @@ set -o errexit
 
 bundle install
 yarn install
-bundle lock --add-platform x86_64-linux
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
