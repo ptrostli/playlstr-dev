afd7dc8bcf6eae3da2d54e37974b749cfd81bf80
Philip Trostli <philiptrostli@gmail.com>
2022-11-28 21:06:58 -0500

    didnt really change anything *shrug*

--- app/javascript/react/components/TempHomePage.js
+++ app/javascript/react/components/TempHomePage.js
@@ -4,8 +4,8 @@ const TempHomePage = (props) => {
 
   return (
     <div className="temp-home">
-      <h1 className="come-back-later">Not Ready For Presentation --- </h1>
-      <h1 className="sorry"> SORRY D:</h1>
+      <h1 className="come-back-later">Not Ready For Presentation---</h1>
+      <h1 className="sorry">SORRY D:</h1>
     </div>
   )
 }
