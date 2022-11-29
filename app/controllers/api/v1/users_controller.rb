6a0e4c7e42c06d70eb9429000e4227c59ca85aee
Philip Trostli <philiptrostli@gmail.com>
2022-11-28 20:09:13 -0500

    woops

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -37,4 +37,5 @@ class Api::V1::UsersController < ApiController
     # Get user's top played artists and tracks
     spotify_user.top_artists #=> (Artist array)
     spotify_user.top_tracks(time_range: 'short_term') #=> (Track array)
+  end
 end
