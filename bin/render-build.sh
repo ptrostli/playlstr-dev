a3c580f906d464170588a19d7f3fca0bcf1d5189
Philip Trostli <philiptrostli@gmail.com>
2022-11-28 20:27:51 -0500

    test

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,6 +5,8 @@ set -o errexit
 
 bundle install
 yarn install
+bundle exec rake db:drop
+bundle exec rake db:create
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
 bundle exec rake db:migrate
